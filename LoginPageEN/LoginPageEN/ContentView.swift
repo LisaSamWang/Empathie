ebea1bc43dab8f8328fa54c4b3f492f3b74d4d8e
Lisa Sam Wang <sam_wang_lisa1121@berkeley.edu>
2021-10-23 13:17:26 -0700

    Removing zip

---
+++ LoginPageEN/LoginPageEN/ContentView.swift
@@ -0,0 +1,129 @@
+//
+//  ContentView.swift
+//  LoginPageEN
+//
+//  Created by Farukh IQBAL on 25/02/2020.
+//  Copyright © 2020 Farukh IQBAL. All rights reserved.
+//
+
+import SwiftUI
+
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0)
+
+let storedUsername = "Farukh"
+let storedpassword = "abcabc"
+
+struct ContentView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    @State var authenticationDidFail: Bool = false
+    @State var authenticationDidSucceed: Bool = false
+    
+    var body: some View {
+        
+        ZStack {
+            VStack {
+                HelloText()
+                UserImage()
+                UsernameTextField(username: $username)
+                PasswordSecureField(password: $password)
+                if authenticationDidFail {
+                    Text("Information not correct. Try again.")
+                        .offset(y: -10)
+                        .foregroundColor(.red)
+                }
+                
+                
+                
+                Button(action: {
+                    if self.username == storedUsername && self.password == storedpassword {
+                        self.authenticationDidSucceed = true
+                        self.authenticationDidFail = false
+                    } else {
+                        self.authenticationDidFail = true
+                        self.authenticationDidSucceed = false
+                    }
+                }) {
+                    LoginButtonContent()
+                }
+            }
+                
+            .padding()
+            if authenticationDidSucceed {
+                Text("Login succeeded!")
+                    .font(.headline)
+                    .frame(width: 250, height: 80)
+                    .background(Color.yellow)
+                    .cornerRadius(20.0)
+                    .animation(Animation.default)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct HelloText: View {
+    var body: some View {
+        Text("Hello you!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct UserImage: View {
+    var body: some View {
+        Image("userImage")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
+    }
+}
+
+struct LoginButtonContent: View {
+    var body: some View {
+        Text("LOGIN")
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 220, height: 60)
+            .background(Color.black)
+            .cornerRadius(35.0)
+    }
+}
+
+struct UsernameTextField: View {
+    
+    @Binding var username: String
+    
+    var body: some View {
+        TextField("Username", text: $username)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+struct PasswordSecureField: View {
+    
+    @Binding var password: String
+    
+    var body: some View {
+        SecureField("Password", text: $password)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
---
+++ LoginPageEN/LoginPageENTests/LoginPageENTests.swift
@@ -0,0 +1,34 @@
+//
+//  LoginPageENTests.swift
+//  LoginPageENTests
+//
+//  Created by Farukh IQBAL on 25/02/2020.
+//  Copyright © 2020 Farukh IQBAL. All rights reserved.
+//
+
+import XCTest
+@testable import LoginPageEN
+
+class LoginPageENTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
